10d5e06b5e568c3ea83334ef510fa9b8a784ce7b
YUWEI HE <148395317+Crystalhe97@users.noreply.github.com>
2024-01-15 22:13:21 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,140 @@
 import streamlit as st
 
-# Set up the title for your website
-st.title("My Personal Website")
+st.set_page_config(
+    page_title="Yuwei He - Student",
+    layout="centered",  # centered or wide
+    initial_sidebar_state="auto",
+)
 
-# Profile Picture
-st.header("Profile Picture")
-# Assuming you have a profile picture named 'profile_pic.jpg' in the same directory
-st.image("profile_pic.jpg", caption='YuweiHe')
+col1, col2 = st.columns([0.3, 0.7])
+with col1:
+    st.markdown(
+        """
+    <style>
+    .profile-img img {
+        width: 100%;
+        border-radius: 50%;
+    }
+    </style>
 
-# About
-st.header("About")
-st.write("Yuwei He - Student")
+    <div class="profile-img">
 
+    ![](https://raw.githubusercontent.com/Crystalhe97/510Lab/main/WechatIMG5371.jpg)
+    </div>
+    """,
+        unsafe_allow_html=True,
+    )
+    # st.image('https://raw.githubusercontent.com/Crystalhe97/510Lab/main/WechatIMG5371.jpg')
+with col2:
+    st.markdown(
+        """
+    # Yuwei He (She/Her)
+                
+    - Student at University of Washington
+        """
+    )
+
+st.markdown(
+    """
 # Education
-st.header("Education")
-st.write("B.A. Communication @UW")
-st.write("B.A. Japanese @UW")
-st.write("M.A. Strategy Communication @JHU")
 
+- [B.A. Communication at University of Washington](https://www.washington.edu)
+- [B.A. Japanese at University of Washington](https://www.washington.edu)
+- [M.A. Strategy Communication at Johns Hopkins University](https://www.jhu.edu)
+"""
+)
+
+st.markdown(
+    """
 # Hobbies and Interests
-st.header("Hobbies")
-st.write("Love eating foods and exploring restaurant")
-# To run the app, save this script as 'app.py' and in the terminal run 'streamlit run app.py'
+- Love cats
+- Love eating and exploring restaurants
+""")
+col1, col2, col3 = st.columns(3)
+
+# Photos want to share
+for col in [col1, col2, col3]:
+    col.markdown(
+        """
+        <style>
+        .profile-img img {
+            width: 100%;
+            border-radius: 10%;
+        }
+        </style>
+
+        <div class="profile-img">
+
+        ![](https://github.com/Crystalhe97/510Lab/blob/main/WechatIMG5325.jpg?raw=true)
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
+
+col1, col2, col3 = st.columns(3)
+
+# Photos want to share
+for col in [col1, col2, col3]:
+    col.markdown(
+        """
+        <style>
+        .profile-img img {
+            width: 100%;
+            border-radius: 10%;
+        }
+        </style>
+
+        <div class="profile-img">
+
+        ![](https://github.com/Crystalhe97/510Lab/blob/main/WechatIMG5321.jpg?raw=true)
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
+
+
+ft = """
+<style>
+a:link , a:visited{
+color: #BFBFBF;  /* theme's text color hex code at 75 percent brightness*/
+background-color: transparent;
+text-decoration: none;
+}
+
+a:hover,  a:active {
+color: #0283C3; /* theme's primary color*/
+background-color: transparent;
+text-decoration: underline;
+}
+
+#page-container {
+  position: relative;
+  min-height: 10vh;
+}
+
+footer{
+    visibility:hidden;
+}
+
+.footer {
+position: relative;
+left: 0;
+top:230px;
+bottom: 0;
+width: 100%;
+background-color: transparent;
+color: #808080; /* theme's text color hex code at 50 percent brightness*/
+text-align: left; /* you can replace 'left' with 'center' or 'right' if you want*/
+}
+</style>
+
+<div id="page-container">
+
+<div class="footer">
+<p style='font-size: 0.875em;'>Made with <a style='display: inline; text-align: left;' href="https://streamlit.io/" target="_blank">Streamlit</a><br 'style= top:3px;'>
+with <img src="https://em-content.zobj.net/source/skype/289/red-heart_2764-fe0f.png" alt="heart" height= "10"/><a style='display: inline; text-align: left;' href="https://github.com/sape94" target="_blank"> by sape94</a></p>
+</div>
+
+</div>
+"""
+st.write(ft, unsafe_allow_html=True)
